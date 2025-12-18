e1a291ef11fbdd4b5313942e1dce9dd7c1e99712
piyush <piyush.khadka2233@gmail.com>
2025-12-18 00:13:33 +0000

    rectangle colliosion

--- main.js
+++ main.js
@@ -1,54 +1,28 @@
-let w = 400;
-let h = 400;
-let x = 190;
-let y = 190;
-let berryx = 150;
-let berryy = 150;
-let berryW = 10;
+let x;
+let y;
+let w = 20;
+let h = 40;
 function setup() {
-  createCanvas(w, h);
+  createCanvas(400, 400);
   background(200);
+  x = width / 2;
+  y = height / 2;
 }
 
 function draw() {
   background(200);
-  fill("red");
-  square(berryx, berryy, berryW);
-  fill("orange");
-  square(x, y, 15);
-  square(x, y + 15, 15);
-}
-
-function keyPressed() {
-  if (key === "w") {
-    y -= 10;
-  } else if (key === "a") {
-    x -= 10;
-  } else if (key === "s") {
-    y += 10;
-  } else if (key === "d") {
-    x += 10;
-  }
-  if (x >= 400) {
-    x = 0;
-  } else if (x <= 0) {
-    x = 400;
-  } else if (y >= 400) {
-    y = 0;
-  } else if (y <= 0) {
-    y = 400;
-  }
-
   if (
-    berryx <= x &&
-    y <= berryy &&
-    berryx + berryW >= x &&
-    berryy + berryW >= y &&
-    x + 15 >= berryx &&
-    y + 15 >= berryy &&
-    berryx + berryW >= x + 15 &&
-    berryy + berryW >= y + 15
+    mouseX <= x + w &&
+    mouseX + w >= x &&
+    mouseY + h >= y &&
+    mouseY <= y + h
   ) {
-    console.log("collied");
+    x = Math.floor(Math.random() * 300) + 1;
+    y = Math.floor(Math.random() * 300) + 1;
   }
+  fill(250, 250, 250);
+  rect(x, y, 20, 20);
+
+  fill(255, 0, 0);
+  rect(mouseX, mouseY, w, h);
 }
