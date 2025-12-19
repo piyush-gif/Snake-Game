f860a7aabdd987bafb23d7343c4895f8bb2aee2d
piyush <piyush.khadka2233@gmail.com>
2025-12-19 16:16:36 +0000

    grid

--- main.js
+++ main.js
@@ -1,28 +1,59 @@
-let x;
-let y;
-let w = 20;
-let h = 40;
+let rows;
+let cols;
+let size = 50;
+let board = [];
+let food;
+let head;
 function setup() {
   createCanvas(400, 400);
-  background(200);
-  x = width / 2;
-  y = height / 2;
+  background(220);
+  rows = floor(width / size);
+  cols = floor(width / size);
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < cols; j++) {
+      board[i][j] = 0;
+    }
+  }
+  food = createVector(
+    Math.floor(Math.random() * 8),
+    Math.floor(Math.random() * 8)
+  );
+  head = createVector(
+    Math.floor(Math.random() * 8),
+    Math.floor(Math.random() * 8)
+  );
+
+  board[food.x][food.y] = -1;
+  board[head.x][head.y] = 1;
 }
 
 function draw() {
-  background(200);
-  if (
-    mouseX <= x + w &&
-    mouseX + w >= x &&
-    mouseY + h >= y &&
-    mouseY <= y + h
-  ) {
-    x = Math.floor(Math.random() * 300) + 1;
-    y = Math.floor(Math.random() * 300) + 1;
-  }
-  fill(250, 250, 250);
-  rect(x, y, 20, 20);
+  background(220);
+  displayGrid();
+}
 
-  fill(255, 0, 0);
-  rect(mouseX, mouseY, w, h);
+function displayGrid() {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (board[i][j] == 0) {
+        fill(255);
+      } else if (board[i][j] == -1) {
+        fill(255, 0, 0);
+      } else if (board[i][j] == 1) {
+        fill(0, 255, 0);
+      }
+      rect(i * size, j * size, size, size);
+    }
+  }
+}
+
+function keyPressed() {
+  if (key == "w") {
+    board.y -= 1;
+  } else if (key == "a") {
+  } else if (key == "s") {
+  } else if (key == "d") {
+  }
 }
