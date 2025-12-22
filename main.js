a999427b0ac919b5deea400b6335f376f7addfea
piyush <piyush.khadka2233@gmail.com>
2025-12-22 15:52:51 +0000

    self grind and snake movement

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 let rows;
 let cols;
-let size = 50;
+let size = 40;
 let board = [];
 let food;
 let head;
 let dir;
 function setup() {
   createCanvas(400, 400);
-  background(220);
-  rows = floor(width / size);
-  cols = floor(width / size);
+  frameRate(60);
+  rows = width / size;
+  cols = height / size;
 
   for (let i = 0; i < rows; i++) {
     board[i] = [];
@@ -17,49 +17,58 @@ function setup() {
       board[i][j] = 0;
     }
   }
-  food = createVector(
-    Math.floor(Math.random() * 8),
-    Math.floor(Math.random() * 8)
-  );
-  head = createVector(
-    Math.floor(Math.random() * 8),
-    Math.floor(Math.random() * 8)
-  );
-
+  food = createVector(int(random(0, rows)), int(random(0, cols)));
+  head = createVector(int(random(0, rows)), int(random(0, cols)));
+  board[food.x][food.y] = -1;
+  board[head.x][head.y] = 1;
   dir = createVector(0, 0);
 }
 
 function draw() {
-  background(220);
-  head.add(dir);
-  board[food.x][food.y] = -1;
+  background(210);
   board[head.x][head.y] = 1;
-  displayGrid();
+
+  createBoard();
 }
 
-function displayGrid() {
+function createBoard() {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      if (board[i][j] == 0) {
-        fill(255);
-      } else if (board[i][j] == -1) {
+      if (board[i][j] == -1) {
         fill(255, 0, 0);
-      } else if (board[i][j] == 1) {
+      } else if (board[i][j] >= 1) {
         fill(0, 255, 0);
+      } else if (board[food.x][food.y] == board[head.x][head.y]) {
+        food = createVector(int(random(0, rows)), int(random(0, cols)));
+      } else {
+        fill(255);
       }
       rect(i * size, j * size, size, size);
     }
   }
 }
+function addTail() {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (board[food.x][food.y] == board[head.x][head.y]) {
+        food = createVector(int(random(0, rows)), int(random(0, cols)));
+      }
+    }
+  }
+}
 
 function keyPressed() {
-  if (key == "w") {
-    dir = createVector(0, -1);
-  } else if (key == "a") {
-    dir = createVector(-1, 0);
+  if (key == "a") {
+    board[head.x][head.y] = 0;
+    head = createVector(head.x - 1, head.y);
+  } else if (key == "w") {
+    board[head.x][head.y] = 0;
+    head = createVector(head.x, head.y - 1);
   } else if (key == "s") {
-    dir = createVector(0, 1);
+    board[head.x][head.y] = 0;
+    head = createVector(head.x, head.y + 1);
   } else if (key == "d") {
-    dir = createVector(1, 0);
+    board[head.x][head.y] = 0;
+    head = createVector(head.x + 1, head.y);
   }
 }
