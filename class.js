cd75f087141ac7a89b3cd22dd81ef6f5e1925271
piyush <piyush.khadka2233@gmail.com>
2026-01-01 20:16:17 +0000

    snake game

--- class.js
+++ class.js
@@ -1,23 +1,63 @@
-class Matrix {
-  constructor(matrix) {
-    this.matrix = matrix;
-  }
+// class Matrix {
+//   constructor(matrix) {
+//     this.matrix = matrix;
+//   }
 
-  addMatrix(matrix2) {
-    for (let i = 0; i < matrix2.length; i++) {
-      for (let j = 0; j < matrix2[0].length; j++) {
-        this.matrix[i][j] += matrix2[i][j];
-      }
-    }
-    return this.matrix;
+//   addMatrix(matrix2) {
+//     for (let i = 0; i < matrix2.length; i++) {
+//       for (let j = 0; j < matrix2[0].length; j++) {
+//         this.matrix[i][j] += matrix2[i][j];
+//       }
+//     }
+//     return this.matrix;
+//   }
+// }
+
+// const mat = [
+//   [1, 2, 3],
+//   [3, 4, 5],
+//   [4, 5, 6],
+// ];
+
+// const matrix1 = new Matrix(mat);
+// console.log(matrix1.addMatrix(mat));
+// let a = [
+//   [1, 1, 1],
+//   [1, 2, 3],
+//   [2, 3, 4],
+// ];
+
+// for (let i = 0; i < 3; i++) {
+//   for (j = 0; j < 3; j++) {
+//     index = a[i][j];
+//     count = 0;
+//     for (k = 0; k < 3; k++) {
+//       if (index == a[j][k]) {
+//         count++;
+//       }
+//     }
+//     if (count == 3) {
+//       console.log("done!");
+//     }
+//   }
+// }
+
+// for (let i = a.length; i >= 0; i--) {
+//   console.log(i);
+// }
+arr = [1, 3, 5, 6, 7, 8, 11];
+target = 11;
+left = 0;
+right = arr.length - 1;
+
+while (left <= right) {
+  mid = Math.floor((left + right) / 2);
+  if (arr[mid] == target) {
+    console.log(true);
+    break;
+  } else if (arr[mid] > target) {
+    right = mid - 1;
+  } else {
+    left = mid + 1;
   }
 }
-
-const mat = [
-  [1, 2, 3],
-  [3, 4, 5],
-  [4, 5, 6],
-];
-
-const matrix1 = new Matrix(mat);
-console.log(matrix1.addMatrix(mat));
--- main.js
+++ main.js
@@ -4,11 +4,14 @@ let size = 40;
 let board = [];
 let head;
 let food;
+let dir;
+let gameOver = false;
+let length = 1;
 function setup() {
   createCanvas(400, 400);
+  frameRate(5);
   rows = width / size;
   cols = height / size;
-
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < cols; j++) {
@@ -17,19 +20,61 @@ function setup() {
   }
   head = createVector(int(random(0, rows)), int(random(0, cols)));
   food = createVector(int(random(0, rows)), int(random(0, cols)));
-  board[head.x][head.y] = 1;
-  board[food.x][food.y] = -1;
+  dir = createVector(0, 0);
 }
 
 function draw() {
   background(220);
+  update();
+  board[food.x][food.y] = -1;
   displayBoard();
+
+  if (gameOver == false) {
+    board[head.x][head.y] = length;
+  }
+}
+
+function update() {
+  head.add(dir);
+  if (dist(head.x, head.y, food.x, food.y) == 0) {
+    generateFood();
+    length += 1;
+  }
+  if (head.x < 0 || head.x > cols - 1 || head.y < 0 || head.y > rows - 1) {
+    gameOver = true;
+    print("GAME OVER");
+    dir.set(0, 0);
+  } else if (board[head.x][head.y] > 1) {
+    gameOver = true;
+    print("GAME OVER");
+    dir.set(0, 0);
+  } else {
+    board[head.x][head.y] = 1 + length;
+    removeTail();
+  }
+}
+function generateFood() {
+  while (true) {
+    food = createVector(int(random(0, rows)), int(random(0, cols)));
+    if (board[food.x][food.y] == 0) {
+      break;
+    }
+  }
+}
+function removeTail() {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (board[i][j] > 0) {
+        board[i][j] -= 1;
+      }
+    }
+  }
 }
 
 function displayBoard() {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      if (board[i][j] == 1) {
+      if (board[i][j] > 0) {
         fill(0, 220, 0);
       } else if (board[i][j] == -1) {
         fill(220, 0, 0);
@@ -43,8 +88,12 @@ function displayBoard() {
 
 function keyPressed() {
   if (key == "w") {
+    dir = createVector(0, -1);
   } else if (key == "s") {
+    dir = createVector(0, 1);
   } else if (key == "a") {
+    dir = createVector(-1, 0);
   } else if (key == "d") {
+    dir = createVector(1, 0);
   }
 }
