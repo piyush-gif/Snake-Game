4753795fc830b32f860d638cb1ae75e2d5a0d118
piyush <piyush.khadka2233@gmail.com>
2026-01-02 17:01:43 +0000

    playing with code snake

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-let rows;
-let cols;
-let size = 40;
+let rows, cols;
 let board = [];
-let head;
-let food;
+let size = 40;
+let head, food;
 let dir;
 let gameOver = false;
 let length = 1;
@@ -12,10 +10,11 @@ function setup() {
   frameRate(5);
   rows = width / size;
   cols = height / size;
+
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < cols; j++) {
-      board[i][j] = 0;
+      board[i][j] = 1;
     }
   }
   head = createVector(int(random(0, rows)), int(random(0, cols)));
@@ -24,64 +23,45 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(180);
   update();
-  board[food.x][food.y] = -1;
-  displayBoard();
-
+  board[food.x][food.y] = 0;
+  createBoard();
   if (gameOver == false) {
-    board[head.x][head.y] = length;
+    board[head.x][head.y] = length + 1;
   }
 }
 
 function update() {
   head.add(dir);
-  if (dist(head.x, head.y, food.x, food.y) == 0) {
-    generateFood();
-    length += 1;
-  }
-  if (head.x < 0 || head.x > cols - 1 || head.y < 0 || head.y > rows - 1) {
+  if (head.x > rows - 1 || head.y > cols - 1 || head.x < 0 || head.y < 0) {
+    print("game Over!");
     gameOver = true;
-    print("GAME OVER");
-    dir.set(0, 0);
-  } else if (board[head.x][head.y] > 1) {
-    gameOver = true;
-    print("GAME OVER");
     dir.set(0, 0);
   } else {
-    board[head.x][head.y] = 1 + length;
     removeTail();
   }
 }
-function generateFood() {
-  while (true) {
-    food = createVector(int(random(0, rows)), int(random(0, cols)));
-    if (board[food.x][food.y] == 0) {
-      break;
-    }
-  }
-}
+
 function removeTail() {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      if (board[i][j] > 0) {
-        board[i][j] -= 1;
-      }
+      board[i][j] -= 1;
     }
   }
 }
 
-function displayBoard() {
+function createBoard() {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      if (board[i][j] > 0) {
-        fill(0, 220, 0);
+      if (board[i][j] == 0) {
+        fill(255, 255, 255);
+      } else if (board[i][j] > 0) {
+        fill(0, 255, 0);
       } else if (board[i][j] == -1) {
-        fill(220, 0, 0);
-      } else {
-        fill(255);
+        fill(140, 0, 0);
       }
-      rect(size * i, size * j, size, size);
+      rect(i * size, j * size, size, size);
     }
   }
 }
@@ -89,10 +69,10 @@ function displayBoard() {
 function keyPressed() {
   if (key == "w") {
     dir = createVector(0, -1);
-  } else if (key == "s") {
-    dir = createVector(0, 1);
   } else if (key == "a") {
     dir = createVector(-1, 0);
+  } else if (key == "s") {
+    dir = createVector(0, 1);
   } else if (key == "d") {
     dir = createVector(1, 0);
   }
